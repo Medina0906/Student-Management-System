9bc52b75da3538748b32833af82980486ef5e43b
Medina0906 <medina.bajgora@student.uni-pr.edu>
2025-02-12 12:53:19 +0100

    Update update_delete.cpp

--- update_delete.cpp
+++ update_delete.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "student.h"
 
+// Funksioni për përditësimin e të dhënave të studentëve
+void updateStudent (std::vector<Student>& students) {
+  int id;
+  std::cout << "\nVendos ID e  studentit që dëshiron të përditësosh: ";
+  std::cin >> id;
+
+  bool found = false;
+  for (auto& student : students) {
+      if (student.id == id) {
+          found = true;
+          std::cout << "Student i gjetur: " << student.name << "\n";
+          std::cout << "Vendos emrin e ri: ";
+          std::cin.ignore();
+          std::getline(std::cin, student.name);
+
+          std::cout << "Vendos moshën e re: ";
+          std::cin >> student.age;
