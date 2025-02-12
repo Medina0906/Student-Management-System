5d9f92d35310d5929d77e07838b9e0480b9bfc75
Medina0906 <medina.bajgora@student.uni-pr.edu>
2025-02-12 11:45:07 +0100

    Update student.h

--- student.h
+++ student.h
@@ -1,1 +1,14 @@
+#ifndef STUDENT_H
+#define STUDENT_H
 
+#include <string>
+
+// Struktura për të ruajtur të dhënat e studentëve
+stuct Student {
+  int id;            // ID e studentit
+  std::string name;  // Emri i studentit
+  int age;           // Mosha e studentit
+  double grade;      // Nota e studentit
+};
+
+#endif // STUDENT_H
