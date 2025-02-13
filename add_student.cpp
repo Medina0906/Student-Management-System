5b69e621dc7f6dcd065bfc390ceadaf7a921621a
djellzaj <djellza.jakupi@student.uni-pr.edu>
2025-02-13 19:44:21 +0100

    Update add_student.cpp
    Përmirësimet më të fundit, përshtatja me anëtarët e grupit.

--- add_student.cpp
+++ add_student.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <iomanip> // Për formatimin e daljes
+#include <student.h>
 
 using namespace std;
 
@@ -17,18 +17,7 @@ struct Student {
 // 2. Vektori global për të ruajtur studentët
 vector<Student> studentList;
 
-// 3. Funksioni për të shfaqur menynë kryesore
-void displayMenu() {
-    cout << "\n===== MENU KRYESOR =====\n";
-    cout << "1. Shto Student\n";
-    cout << "2. Shfaq Studentet\n";
-    cout << "3. Kerko Student\n";
-    cout << "4. Fshi Student\n";
-    cout << "5. Dal\n";
-    cout << "Zgjedhja juaj: ";
-}
-
-// 4. Funksioni për të shtuar një student të ri
+// 3. Funksioni për të shtuar një student të ri
 void addStudent() {
     Student newStudent; // Krijimi i një objekti të ri të tipit Student
 
@@ -57,95 +46,40 @@ void addStudent() {
     cout << "Nota Mesatare: ";
     cin >> newStudent.notaMesatare;
 
-    // Shtojmë studentin e ri në vektor
+    // Shto studentin e ri në vektor
     studentList.push_back(newStudent);
 
     cout << "Studenti u shtua me sukses!\n";
 }
 
-// 5. Funksioni për të shfaqur të gjithë studentët
-void displayStudents() {
-    // Kontrollojmë nëse lista është bosh
-    if (studentList.empty()) {
-        cout << "\nNuk ka studentë të regjistruar!\n";
-        return;
-    }
-
-    // Shfaqja e titullit të listës
-    cout << "\n===== LISTA E STUDENTËVE =====\n";
-    cout << setw(5) << "ID" << setw(15) << "Emri" << setw(15) << "Mbiemri" << setw(10) << "Mosha" << setw(15) << "Nota Mesatare\n";
-    cout << "-------------------------------------------------\n";
-
-    // Shfaq të dhënat e secilit student
-    for (const auto& student : studentList) {
-        cout << setw(5) << student.id << setw(15) << student.emri << setw(15) << student.mbiemri
-             << setw(10) << student.mosha << setw(15) << student.notaMesatare << endl;
-    }
-}
-
-// 6. Funksioni për të kërkuar një student sipas ID
-void searchStudent() {
-    int id;
-    cout << "\n===== KERKO STUDENT =====\n";
-    cout << "Shkruaj ID-në e studentit: ";
-    cin >> id;
-
-    // Kërkojmë studentin në listë
-    for (const auto& student : studentList) {
-        if (student.id == id) {
-            // Nëse studenti gjendet, shfaq detajet
-            cout << "\nStudenti u gjet:\n";
-            cout << "ID: " << student.id << ", Emri: " << student.emri << ", Mbiemri: " << student.mbiemri
-                 << ", Mosha: " << student.mosha << ", Nota Mesatare: " << student.notaMesatare << endl;
-            return;
-        }
-    }
-
-    // Nëse studenti nuk gjendet
-    cout << "Studenti me ID " << id << " nuk u gjet.\n";
-}
-
-// 7. Funksioni për të fshirë një student sipas ID
-void deleteStudent() {
-    int id;
-    cout << "\n===== FSHI STUDENT =====\n";
-    cout << "Shkruaj ID-në e studentit për të fshirë: ";
-    cin >> id;
-
-    // Kërkojmë studentin në listë dhe e fshijmë
-    for (auto it = studentList.begin(); it != studentList.end(); ++it) {
-        if (it->id == id) {
-            studentList.erase(it); // Fshi studentin
-            cout << "Studenti u fshi me sukses!\n";
-            return;
-        }
-    }
-
-    // Nëse studenti nuk gjendet
-    cout << "Studenti me ID " << id << " nuk u gjet.\n";
-}
-
-// 8. Funksioni kryesor
+// 4. Funksioni kryesor
 int main() {
     int choice;
 
     while (true) {
-        displayMenu(); // Shfaqja e menysë
+        // Paraqesim menynë e thjeshtë
+        cout << "\n===== MENU KRYESOR =====\n";
+        cout << "1. Shto Student\n";
+        cout << "2. Shfaq Studentet (Punë e ekipit tjetër)\n";
+        cout << "3. Kerko Student (Punë e ekipit tjetër)\n";
+        cout << "4. Fshi Student (Punë e ekipit tjetër)\n";
+        cout << "5. Dal\n";
+        cout << "Zgjedhja juaj: ";
         cin >> choice;
 
         // Menaxhimi i zgjedhjes së përdoruesit
         switch (choice) {
             case 1:
-                addStudent(); // Shtimi i studentit
+                addStudent(); // Shtimi i studentit (pjesa jote)
                 break;
             case 2:
-                displayStudents(); // Shfaqja e studentëve
+                cout << "\nShfaqja e studentëve është pjesë e punës së ekipit tjetër.\n";
                 break;
             case 3:
-                searchStudent(); // Kërkimi i studentit
+                cout << "\nKërkimi i studentëve është pjesë e punës së ekipit tjetër.\n";
                 break;
             case 4:
-                deleteStudent(); // Fshirja e studentit
+                cout << "\nFshirja e studentëve është pjesë e punës së ekipit tjetër.\n";
                 break;
             case 5:
                 cout << "Duke dalë nga programi...\n";
